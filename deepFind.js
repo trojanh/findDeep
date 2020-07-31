ccfc4ac612e94bd8308eaffba247ba75d6250510
Rajan Tiwari <22974490+trojanh@users.noreply.github.com>
2020-07-31 13:09:46 +0530

    Allow multiple values for searchKey

--- deepFind.js
+++ deepFind.js
@@ -1,12 +1,14 @@
 function deepFind(objectToSearch, searchKey) {
-  if(typeof objectToSearch !== 'object') return null
-  if(objectToSearch[searchKey]) return objectToSearch[searchKey]
+  if (typeof objectToSearch !== 'object') return null
+  const foundValues = []
+  if (objectToSearch[searchKey]) foundValues.push(objectToSearch[searchKey])
+
   // works for arrays and objects
   for (const iterator of Object.values(objectToSearch)) {
     const result = deepFind(iterator, searchKey)
-    if(result !== null) return result
+    if (result !== null) foundValues.push(...result)
   }
-  return null
+  return foundValues
 }
 
 module.exports = deepFind
