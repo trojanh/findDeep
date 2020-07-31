3c79627f2846c82aa5fcbffe12f44b7bd452b1e5
Rajan Tiwari <22974490+trojanh@users.noreply.github.com>
2020-07-31 11:45:48 +0530

    Refactor and remove repeated code

--- deepFind.js
+++ deepFind.js
@@ -1,15 +1,9 @@
-
 function deepFind(objectToSearch, searchKey) {
   if(typeof objectToSearch !== 'object') return null
   if(objectToSearch[searchKey]) return objectToSearch[searchKey]
-  if(Array.isArray(objectToSearch)) {
-    for (const iterator of objectToSearch) {
-      const result = deepFind(iterator, searchKey)
-      if(result !== null) return result
-    }
-  }
-  for (const objectKey in objectToSearch) {
-    const result = deepFind(objectToSearch[objectKey], searchKey)
+  // works for arrays and objects
+  for (const iterator of Object.values(objectToSearch)) {
+    const result = deepFind(iterator, searchKey)
     if(result !== null) return result
   }
   return null
