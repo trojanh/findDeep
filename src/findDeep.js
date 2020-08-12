e6155fb508429ae27333dea17527f7fcc21aa42c
trojanh <rajan.tiwari@kiprosh.com>
2020-08-12 16:42:08 +0530

    Add nested and multi key search

--- src/findDeep.js
+++ src/findDeep.js
@@ -1,10 +1,12 @@
+// @ts-check
 'use strict'
 
 /**
  * Find deeply nested keys in object using searchKey
  * @author Rajan Tiwari <tiwarirajan@hotmail.com>
- * @param {(array|object)} objectToSearch Object or Array on which search needs to performed
- * @param {String} searchKey Object key to be searched in objectToSearch
+ * @param {object | []} objectToSearch Object or Array on which search needs to performed
+ * @param {String | [String]} searchKey Object key to be searched in objectToSearch
+ * @param {{ nested: Boolean}} options Optional object to search nested keys, is false by default
  * @returns {array} Found value for searchKey, could be any type of value.
  *
  * @example
@@ -32,15 +34,45 @@
  * //     { id: '1004', type: "Devil's Food" }
  * //  ]
  */
-function findDeep(objectToSearch, searchKey) {
+function findDeep(objectToSearch, searchKey, options = { nested: false  }) {
+  const { nested = false } = options
+  if (!searchKey) return objectToSearch
+  if (typeof(searchKey) === 'string') return findDeepKeys(objectToSearch, [searchKey])
+
+  const isArray = typeof(searchKey) === 'object' && Array.isArray(searchKey)
+
+  if (isArray && nested) return findDeepNestedKeys(objectToSearch, searchKey)
+  if (isArray) return findDeepKeys(objectToSearch, searchKey)
+  throw new Error('Invalid searchKey')
+}
+
+function findDeepKeys(objectToSearch, searchKeys) {
   if (!objectToSearch || typeof objectToSearch !== 'object') return null
-  const foundValues = []
-  if (objectToSearch[searchKey]) foundValues.push(objectToSearch[searchKey])
+  const foundValues = [...objectKeyValue(objectToSearch, searchKeys)  ]
 
   // works for arrays and objects
   for (const iterator of Object.values(objectToSearch)) {
-    const result = findDeep(iterator, searchKey)
+    const result = findDeepKeys(iterator, searchKeys)
     if (result !== null) foundValues.push(...result)
+  }
+  return foundValues
+}
+
+function objectKeyValue(objectToSearch, searchKeys = []) {
+  const objectValues = []
+  searchKeys.forEach(searchKey => {
+    const objectValue = objectToSearch[searchKey]
+    if(objectValue) objectValues.push(objectValue)
+  })
+  return objectValues
+}
+
+function findDeepNestedKeys(objectToSearch, searchKeys) {
+  let foundValues = objectToSearch
+
+  for (const searchKey of searchKeys) {
+    foundValues = findDeepKeys(foundValues, [searchKey])
+    if (foundValues === null) return []
   }
   return foundValues
 }
